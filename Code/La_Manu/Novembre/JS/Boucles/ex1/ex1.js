1a4a2a490b598c106f51dc6876fe481a96d8d3fe
ClementG60 <84459714+ClementG60@users.noreply.github.com>
2021-11-03 10:01:35 +0100

    Delete Code/La_Manu/Novembre/JS/Boucles directory

--- Code/La_Manu/Novembre/JS/Boucles/ex1/ex1.js
+++
@@ -1,7 +0,0 @@
-for (let year = 2020; year < 2031; year++) {
-    if ((year % 4 == 0) && ((year % 100 != 0)) || (year % 400 == 0)) {
-        document.write((year + " ").fontcolor("red"));
-    } else {
-        document.write(year + " ");
-    }
-}
